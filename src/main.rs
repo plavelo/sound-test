fc0ec195e4fa975700fa68c3a3343519d9062264
plavelo <plastic.velocity@gmail.com>
2025-07-07 14:23:36 +0900

    Add organ sound

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,122 @@
+#![allow(clippy::precedence)]
+
+use assert_no_alloc::*;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{FromSample, SizedSample};
+use fundsp::hacker::*;
+use once_cell::race::OnceBox;
+use std::sync::Arc;
+
+#[cfg(debug_assertions)] // required when disable_release is set (default)
+#[global_allocator]
+static A: AllocDisabler = AllocDisabler;
+
 fn main() {
-    println!("Hello, world!");
+    let host = cpal::default_host();
+
+    let device = host
+        .default_output_device()
+        .expect("Failed to find a default output device");
+    let config = device.default_output_config().unwrap();
+
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()).unwrap(),
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()).unwrap(),
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()).unwrap(),
+        _ => panic!("Unsupported format"),
+    }
+}
+
+fn organ_table() -> Arc<Wavetable> {
+    static INSTANCE: OnceBox<Arc<Wavetable>> = OnceBox::new();
+    INSTANCE
+        .get_or_init(|| {
+            let table = Wavetable::new(
+                20.0,
+                20_000.0,
+                4.0,
+                // Set phase to enable interpolation with saw, triangle and soft saw wavetables.
+                &|i| {
+                    if (i & 3) == 3 {
+                        0.5
+                    } else if (i & 1) == 1 {
+                        0.0
+                    } else {
+                        0.5
+                    }
+                },
+                &|_, i| {
+                    let z = i.trailing_zeros();
+                    let j = i >> z;
+                    1.0 / (i + j * j * j) as f64
+                },
+            );
+            Box::new(Arc::new(table))
+        })
+        .clone()
+}
+
+fn organ() -> An<WaveSynth<U1>> {
+    An(WaveSynth::new(organ_table()))
+}
+
+fn organ_hz(f: f32) -> An<Pipe<Constant<U1>, WaveSynth<U1>>> {
+    constant(f) >> organ()
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate.0 as f64;
+    let channels = config.channels as usize;
+
+    let c = 0.2 * (organ_hz(midi_hz(57.0)) + organ_hz(midi_hz(61.0)) + organ_hz(midi_hz(64.0)));
+
+    let c = c >> pan(0.0);
+
+    // Add chorus.
+    let c = c >> (chorus(0, 0.0, 0.01, 0.2) | chorus(1, 0.0, 0.01, 0.2));
+
+    let mut c = c >> (declick() | declick()) >> (dcblock() | dcblock()) >> limiter_stereo(1.0, 5.0);
+
+    c.set_sample_rate(sample_rate);
+    c.allocate();
+
+    let mut next_value = move || assert_no_alloc(|| c.get_stereo());
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {err}");
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(50000));
+
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> (f32, f32))
+where
+    T: SizedSample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let sample = next_sample();
+        let left = T::from_sample(sample.0);
+        let right: T = T::from_sample(sample.1);
+
+        for (channel, sample) in frame.iter_mut().enumerate() {
+            if channel & 1 == 0 {
+                *sample = left;
+            } else {
+                *sample = right;
+            }
+        }
+    }
 }
